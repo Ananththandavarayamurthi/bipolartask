52cbd526e09a9f51fffa69e1e62fdd493e83e177
Ananththandavarayamurthi <ananthcivil9876@gmail.com>
2023-08-26 18:14:00 +0530

    functionality

--- src/components/CreateNotes.js
+++ src/components/CreateNotes.js
@@ -6,6 +6,32 @@ import store from '../store/store';
 function CreateNotes() {
   const [title, setTitle] = useState('');
   const [description, setDescription] = useState('');
+  const inputBaseStyle = {
+    color: "#00ffff",
+    border: "#00ffff solid 0.5px",
+    borderRadius: "10px",
+    height: "40px",
+    alignItems: "flex-start", 
+    display: "flex", 
+    padding: "5px",
+    width: "400px",
+    transition: "box-shadow 0.3s ease-in-out", // Add smooth transition
+    '&:hover': {
+      boxShadow: "0 0 8px rgba(0, 255, 255, 0.6)", // Box shadow on hover
+    },
+  };
+  const inputBaseStyle2 = {
+    color: "#00ffff",
+    border: "#00ffff solid 0.5px",
+    borderRadius: "10px",
+    height: "200px",
+    alignItems: "flex-start", 
+    display: "flex", 
+    padding: "5px",
+    width: "400px",
+    transition: "box-shadow 0.3s ease-in-out",
+    
+  };
 
   const handleAddNote = (e) => {
     e.preventDefault();
@@ -29,15 +55,22 @@ function CreateNotes() {
           <InputLabel style={{ color: "#00ffff" ,gap:"2px"}} shrink htmlFor="title-input">
             Title
           </InputLabel>
-          <InputBase id="title-input" style={{ color: "#00ffff",border:"#00ffff solid 0.5px" ,borderRadius:"10px",hegith:"70px" }} value={title} onChange={(e) => setTitle(e.target.value)} />
+          <InputBase id="title-input"
+           style={inputBaseStyle}
+           value={title} onChange={(e) => setTitle(e.target.value)} />
         </FormControl>
         <FormControl variant="outlined" style={{padding:"5px",width:"400px"}}>
           <InputLabel style={{ color: "#00ffff" ,padding:"2px"}} shrink htmlFor="description-input">
             Description
           </InputLabel>
-          <InputBase id="description-input" style={{ color: "#00ffff",border:"#00ffff solid 0.5px" ,borderRadius:"10px",hegith:"70px" } }} value={description} onChange={(e) => setDescription(e.target.value)} />
+          <InputBase id="description-input" 
+          style={inputBaseStyle2} 
+          value={description} onChange={(e) => setDescription(e.target.value)} />
         </FormControl>
-        <Button id="button-input" style={{ backgroundColor: "#00ffff", color: "#ffffff" }} type="submit">
+        <Button id="button-input" style={{ backgroundColor: "#00ffff", color: "#ffffff",transition: "box-shadow 0.3s ease-in-out", // Add smooth transition
+    '&:hover': {
+      boxShadow: "0 0 8px rgba(0, 255, 255, 0.6)", // Box shadow on hover
+    } }} type="submit">
           Add Note
         </Button>
       </form>
--- src/components/Notes.js
+++ src/components/Notes.js
@@ -1,5 +1,8 @@
 import React from 'react'
 import "./component.css"
+import {Link} from "react-router-dom"
+
+
 
 function Notes({ title, description, date }) {
   return (
@@ -9,6 +12,13 @@ function Notes({ title, description, date }) {
       <h2 variant="h6">{title}</h2>
       <p className='p1'>{description}</p>
       <p className='p2'>{date}</p>
+      <div className="ii">
+				<Link to={`/notes`}>
+					<button title='Details' className='detailbutton'>Details</button>
+				</Link>
+
+				<button className='detailbutton' onClick={hanleDelete}>delete</button>
+			</div>
     </div>
     </>
   )
